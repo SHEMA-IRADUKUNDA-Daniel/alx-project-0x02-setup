896a0ff25ac2057bbc2fbf0b6a9788beda8b0c62
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-07-26 16:18:31 +0200

    renaming posts from post and importing header into about page

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Button from "@/components/common/Button";
+import Header from "@/components/layout/Header";
 
 const AboutPage = () => {
   return (
     <div className="p-8 h-500 bg-white text-center]">
-      <h1 className="text-3xl text-center font-bold text-black">About Us</h1>
+      <Header />
+      <h1 className="text-3xl my-10 text-center font-bold text-black">
+        About Us
+      </h1>
       <Button size="medium" shape="rounded-md" />
     </div>
   );
