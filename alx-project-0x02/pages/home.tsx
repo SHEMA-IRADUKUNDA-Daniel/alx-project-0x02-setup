efb50d55269c75b71cf8f4220eb10bd99d65ae22
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-07-26 15:21:32 +0200

    creating resusable card component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Card from "@/components/layout/Card";
 
 const HomePage = () => {
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold">Welcome to the Home Page</h1>
+      <Card
+        title="Hello from Home Page"
+        content="Wake up to the sound of waves in this stunning oceanfront property."
+      ></Card>
     </div>
   );
 };
