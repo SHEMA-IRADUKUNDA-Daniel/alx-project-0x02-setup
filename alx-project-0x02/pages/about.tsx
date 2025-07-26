550b8e532319307cd8975c2c127f7f243e917f25
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-07-26 16:20:56 +0200

    importing posts in header and changing about content

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -4,12 +4,14 @@ import Header from "@/components/layout/Header";
 
 const AboutPage = () => {
   return (
-    <div className="p-8 h-500 bg-white text-center]">
-      <Header />
-      <h1 className="text-3xl my-10 text-center font-bold text-black">
-        About Us
-      </h1>
-      <Button size="medium" shape="rounded-md" />
+    <div>
+      <div className="p-8 h-500 bg-white text-center]">
+        <Header />
+        <h1 className="text-3xl my-10 text-center font-bold text-black">
+          About Us
+        </h1>
+        <Button size="medium" shape="rounded-md" />
+      </div>
     </div>
   );
 };
